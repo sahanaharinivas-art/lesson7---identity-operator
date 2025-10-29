50b5ab019afe1f75d3c13920258b8b312f3975e9
harinivassahana-max <harinivassahana@gmail.com>
2025-10-30 05:42:35 +1100

    assignment 7

---
+++ assignment 7.py
@@ -0,0 +1,7 @@
+# get input from user
+char = input("enter a character: ")
+
+# use ord() function to get ASCII value
+ascii_value = ord(char)
+
+print(f" the ASCII value of '(char)' is (ascii_value)")
